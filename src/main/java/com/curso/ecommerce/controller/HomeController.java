ec8cddba2ffab98249cc0d7f959823b15c4e22d5
DavidTrejos <davidtrejoscortes@gmail.com>
2023-07-18 14:55:52 -0500

    Redirección al carrito de compras

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.curso.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.service.ProductoService;
 
 @Controller
@@ -30,9 +34,20 @@ public class HomeController {
 	
 	@GetMapping("productohome/{id}")
 	
-	public String productoHome(@PathVariable Integer id) {
+	public String productoHome(@PathVariable Integer id, Model model) { //El objeto model nos permite llevar información  hacia la vista
 		log.info("Id producto enviado como parámetro {}",id);
+		Producto producto = new  Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+		
+		model.addAttribute("producto", producto);
+		
 		return "usuario/productohome";
+	}
+	
+	@PostMapping("/cart")
+	public String addCart() {
+		return "usuario/carrito";
 	}
 	
 	
