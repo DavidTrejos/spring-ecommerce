eaff0e69cf23254fee6d3353283e73d0b2ea125b
DavidTrejos <davidtrejoscortes@gmail.com>
2023-07-18 18:14:54 -0500

    Añadimos la redirección a la vista resumen orden

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -74,7 +74,16 @@ public class HomeController {
 		detalleOrden.setTotal(producto.getPrecio() * cantidad);
 		detalleOrden.setProducto(producto);
 		
-		detalles.add(detalleOrden);
+		//Validar que el producto no se añada dos veces.
+		Integer idProducto = producto.getId();
+		boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+		
+		if(!ingresado) {
+			detalles.add(detalleOrden);
+		}
+		
+		
+		
 
 		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 
@@ -85,33 +94,45 @@ public class HomeController {
 
 		return "usuario/carrito";
 	}
-	
-	
-	//Quitar un producto del carrito
+
+	// Quitar un producto del carrito
 	@GetMapping("/delete/cart/{id}")
 	public String deleteProductoCart(@PathVariable Integer id, Model model) {
-		//Lista nueva de productos
-		List <DetalleOrden> ordenesNueva = new ArrayList <DetalleOrden>();
-		for(DetalleOrden detalleOrden: detalles) {
-			if(detalleOrden.getProducto().getId()!=id) {
+		// Lista nueva de productos
+		List<DetalleOrden> ordenesNueva = new ArrayList<DetalleOrden>();
+		for (DetalleOrden detalleOrden : detalles) {
+			if (detalleOrden.getProducto().getId() != id) {
 				ordenesNueva.add(detalleOrden);
 			}
 		}
-		//Poner la neuva lista con los productos restantes.
-		detalles= ordenesNueva;
-		
-		double sumaTotal=0;
-		
-		
+		// Poner la nueva lista con los productos restantes.
+		detalles = ordenesNueva;
+
+		double sumaTotal = 0;
+
 		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 
 		orden.setTotal(sumaTotal);
-
 		model.addAttribute("cart", detalles);
 		model.addAttribute("orden", orden);
-		
-		
+
 		return "usuario/carrito";
+	}
+	
+	
+	@GetMapping ("/getCart")
+	public String getCart(Model model) {
+		
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		return "/usuario/carrito";
+	}
+	
+	@GetMapping("/order")
+	public String order() {
+		
+		
+		return "usuario/resumenorden";
 	}
 
 }
