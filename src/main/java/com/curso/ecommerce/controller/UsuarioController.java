1a931d98aacfdadfb6f36404dc7db7a65e9964a9
DavidTrejos <davidtrejoscortes@gmail.com>
2023-07-20 13:55:44 -0500

    Obtener usuario login y objeto  session de tipo http

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.curso.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,32 +13,56 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.curso.ecommerce.model.Usuario;
 import com.curso.ecommerce.service.IUsuarioService;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
-	
-	
-		  	private final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
-	
-			@Autowired
-			private IUsuarioService usuarioService;
-			
-			// /usuario/registro  ----> Creación de la ruta
-			@GetMapping("/registro")
-			public String create() {
-				return "usuario/registro";
-			}
-			
-			@PostMapping("/save")
-			public String save(Usuario usuario) {
-				logger.info("Usuario registro: {}",usuario);
-				
-				usuario.setTipo("USER");
-				usuarioService.save(usuario);
+
+	private final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
+
+	@Autowired
+	private IUsuarioService usuarioService;
+
+	// /usuario/registro ----> Creación de la ruta
+	@GetMapping("/registro")
+	public String create() {
+		return "usuario/registro";
+	}
+
+	@PostMapping("/save")
+	public String save(Usuario usuario) {
+		logger.info("Usuario registro: {}", usuario);
+
+		usuario.setTipo("USER");
+		usuarioService.save(usuario);
+		return "redirect:/";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "usuario/login";
+	}
+
+	@PostMapping("/acceder")
+	public String acceder(Usuario usuario, HttpSession session) {
+		logger.info("Accesos: {}", usuario);
+
+		Optional<Usuario> user = usuarioService.findByEmail(usuario.getEmail());
+		logger.info("Usuario de db: {}", user.get());
+
+		if (user.isPresent()) {
+
+			session.setAttribute("idusuario", user.get().getId());
+			if (user.get().getTipo().equals("ADMIN")) {
+				return "redirect:/admin";
+			} else {
 				return "redirect:/";
 			}
-			
-			
-			
+		} else {
+			logger.info("Usuario no existe");
+		}
+		return "redirect:/";
+	}
 
 }
