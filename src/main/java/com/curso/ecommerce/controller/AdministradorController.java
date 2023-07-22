05c0afe62fce3cf69e8be3b269277c02139bbc5d
DavidTrejos <davidtrejoscortes@gmail.com>
2023-07-22 13:29:01 -0500

    Funcionalidad para ver usuarios admin

--- src/main/java/com/curso/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/curso/ecommerce/controller/AdministradorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.model.Producto;
+import com.curso.ecommerce.service.IUsuarioService;
 import com.curso.ecommerce.service.ProductoService;
 
 @Controller
@@ -17,6 +18,10 @@ public class AdministradorController {
 	
 	 			@Autowired
 				private ProductoService productoService;
+	 			
+	 			@Autowired
+	 			private IUsuarioService usuarioService;
+	 			
 	     //Integración del home al administrador
 	
 					@GetMapping("")
@@ -26,4 +31,10 @@ public class AdministradorController {
 						model.addAttribute("productos", productos);
 	 					return "administrador/home";
 	 				}
+					
+					@GetMapping("/usuarios")
+					public String usuarios(Model model) {
+						model.addAttribute("usuarios", usuarioService.findAll());
+						return "administrador/usuarios";
+					}
 }
